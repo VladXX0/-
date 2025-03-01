88280ef1fb9cd1e98d85a387450ebccc857b66dd
VladXX0 <149883216+VladXX0@users.noreply.github.com>
2025-03-01 19:35:47 +0300

    Boom them all

--- pythonProject2/у.py
+++ pythonProject2/у.py
@@ -1,44 +1,53 @@
 import pygame
-from os import path
 from sys import exit
+from os import path
+from random import randrange
 
 
 def load_image(name):
     fullname = path.join('data', name)
     if not path.exists(fullname):
-        print(f'Не удалось найти файл {fullname}')
+        print(f'Не найден файл {fullname}')
         exit(1)
     image = pygame.image.load(fullname)
     return image
 
 
-if __name__ == '__main__':
+class Bomb(pygame.sprite.Sprite):
+    bomb_image = load_image('bomb.png')
+    boom_image = load_image('boom.png')
+
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.image = Bomb.bomb_image
+        self.rect = self.image.get_rect()
+        self.rect.x = randrange(0, 450)
+        self.rect.y = randrange(0, 449)
+
+    def update(self, *args):
+        if args and args[0].type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(args[0].pos):
+            self.image = Bomb.boom_image
+
+
+if __name__ == "__main__":
     pygame.init()
-    pygame.display.set_caption('Герой двигается!')
-    size = width, height = 300, 300
-    screen = pygame.display.set_mode(size)
-    is_running = True
+    pygame.display.set_caption('Boom them all')
+    sprites = pygame.sprite.Group()
+    for i in range(20):
+        Bomb(sprites)
     clock = pygame.time.Clock()
-    player_x = 0
-    player_y = 0
-    speed = 10
-    creature = load_image('creature.png')
+    is_running = True
+    size = width, height = 500, 500
+    screen = pygame.display.set_mode(size)
     while is_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_running = False
                 break
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    player_y -= speed
-                elif event.key == pygame.K_DOWN:
-                    player_y += speed
-                elif event.key == pygame.K_RIGHT:
-                    player_x += speed
-                elif event.key == pygame.K_LEFT:
-                    player_x -= speed
-        screen.fill('white')
-        screen.blit(creature, (player_x, player_y))
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                sprites.update(event)
+        screen.fill('black')
+        sprites.draw(screen)
         pygame.display.flip()
         clock.tick(60)
     pygame.quit()
